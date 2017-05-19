38d4764d3ab2078e467d0688e800ad56cb64dbe5
wodeduoer <wodeduoer@163.com>
2017-05-19 15:39:13 +0800

    20170519:jinzi:add a new line in order to test git commands

--- myfirstpy.py
+++ myfirstpy.py
@@ -1,6 +1,16 @@
+""""
+docstring
+"""
 
-print('Hello,World')
 
-print('It is my first python!')
+def fib(n):
+    a, b, c = 0, 1, 0
+    while a < n:
+        # print(a, end=' ')
+        a, b = b, a + b
+        c = c + 1
+        print("a:", a, "b:", b, "c:", c)
+    print()
 
-print("And I'm test git commmmmmmmmmmmmmmmands")
+
+fib(1000)
